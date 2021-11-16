702dad902ef5409911dde3d213cafbe2f61c76c1
musai7 <musaishankar307@gmail.com>
2021-11-16 22:07:48 +0530

    added : simple linkedList

---
+++ src/com/bridgeit/linkedlist/LinkedListPracticeMain.java
@@ -0,0 +1,14 @@
+package com.bridgeit.linkedlist;
+
+public class LinkedListPracticeMain {
+	
+	public static void main(String[] args) {
+		
+		LinkedListPractice linkedListPractice = new LinkedListPractice();
+		linkedListPractice.add(56);
+		linkedListPractice.add(30);
+		linkedListPractice.add(70);
+		linkedListPractice.toPrint();
+	}
+
+}
